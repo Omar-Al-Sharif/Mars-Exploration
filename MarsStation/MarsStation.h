ea9e025c72595e6d41992e54ba5d03e79c7801dc
legendarystatus01 <82820327+legendarystatus01@users.noreply.github.com>
2021-05-31 23:57:22 +0200

    Update MarsStation.h

--- MarsStation/MarsStation.h
+++ MarsStation/MarsStation.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "Queue.h"
-
+#include "Event.h"
+#include "UI.h"
+#include "Mission.h"
 
 class MarsStation
 {
 private:
-	LinkedQueue<Events> EventList;
+	LinkedQueue<Event *> EventList;
+	LinkedQueue<Mission> MissionList;
+	int currentDay; //the day we are in
+	UI userInterface;
 public:
-	void ExecuteAll()
-	{
-			
-	}
+	void ExecuteAll();
 };
