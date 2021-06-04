788bdb6895b73353d1aedafed817d10ec4957c98
legendarystatus01 <82820327+legendarystatus01@users.noreply.github.com>
2021-06-04 20:04:14 +0200

    Update MarsStation.h

--- MarsStation/MarsStation.h
+++ MarsStation/MarsStation.h
@@ -3,14 +3,22 @@
 #include "Event.h"
 #include "UI.h"
 #include "Mission.h"
+#include "Rovers.h"
+#include"ERovers.h"
+#include"PRovers.h"
 
 class MarsStation
 {
 private:
 	LinkedQueue<Event *> EventList;
-	LinkedQueue<Mission> MissionList;
+	LinkedQueue<Mission> PMissionList;
+	LinkedQueue<ERover> AvailableERoverList;
+	LinkedQueue<PRover> AvailablePRoverList;
+
 	int currentDay; //the day we are in
-	UI userInterface;
+	UI userInterface; //
 public:
 	void ExecuteAll();
+	void outputSim();
+	void CallOutputChoice();
 };
