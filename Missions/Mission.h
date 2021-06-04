18c65bf5eadac42952b8010f1b0640ca4fc9a811
legendarystatus01 <82820327+legendarystatus01@users.noreply.github.com>
2021-06-04 21:25:46 +0200

    Update Mission.h

--- Missions/Mission.h
+++ Missions/Mission.h
@@ -16,15 +16,18 @@ private:
 	int Significance;
 	// Equation of consumptionDays = 2*(TargetLocation*RoverSpeed)/25 +MD;
 	Rovers* R; //Rover assigned to the mission
-	
+	char missionType;
 	int missionID;
 	static int numberOfmissions;
 
 public:
-	Mission(int fd, int md, int targetloc, int significance);
+	Mission(int fd, int md, int targetloc, int significance,int Id,char MT);
 	void assigntoRover(Rovers* R);
 	void setWaitingDays(int wd);
+	
+
 	int getFinishDay() const;
+	int getNumberOfmissions() const;
 	int getFormulationDay() const;
 	int getMissionId() const;
 	int getWaitingDays() const;
