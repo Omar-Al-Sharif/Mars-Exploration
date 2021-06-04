dea7522afe62acc59fea7164d11b3cfbff23aab1
legendarystatus01 <82820327+legendarystatus01@users.noreply.github.com>
2021-06-04 22:08:30 +0200

    Update FormulationEvent.cpp

--- Events/FormulationEvent.cpp
+++ Events/FormulationEvent.cpp
@@ -1,8 +1,9 @@
 #include "FormulationEvent.h"
-FormulationEvent::FormulationEvent(char m, int x, int y, float t, float f, int s):Event(x,y) {
+FormulationEvent::FormulationEvent(char m, int d, int missionId, float Location, float f, int s) :Event(missionId,d)
+{
 	Mtype = m;
-	Tlocation = t;
-	Fmission = f;
+	Tlocation = Location;
+	missionDuration = f;
 	sig = s;
 }
 void FormulationEvent::Execute() {
