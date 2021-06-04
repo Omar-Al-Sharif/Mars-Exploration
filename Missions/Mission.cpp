2bce52926e33deb12ca0fc3220c3f2028cc3eee0
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-06-04 15:48:06 +0200

    Add files via upload

---
+++ Missions/Mission.cpp
@@ -0,0 +1,27 @@
+#include "Mission.h"
+
+Mission::Mission(int fd, int md, int targetloc)
+{
+	FD = fd;
+	MD = md;
+	targetlocation = targetloc;
+}
+
+void Mission::setWaitingDays(int wd)
+{
+	WD = wd;
+}
+
+void Mission::assigntoRover(Rovers* R)
+{
+	this->R = R;
+	int speed = R->getSpeed();
+	ED = 2 * (targetlocation * speed) / 25 + MD;
+	CD = WD + ED;  //Completion day is the summation of waiting days + execution days 
+	// Because execution day aleady includes the mission duration 
+}
+
+int Mission::getFinishDay() const
+{
+	return CD;
+}
