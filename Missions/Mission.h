ac899d1753cea72312285669af2f7a3ee44d8f7a
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-06-04 15:49:02 +0200

    Update Mission.h

--- Missions/Mission.h
+++ Missions/Mission.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "../Data structure and algorithm/ArrayStack.h"
 #include "Rovers.h"
-
 
 class Mission
 {
@@ -10,14 +8,17 @@ class Mission
 private:
 	int FD; //Formulation day of the mission
 	int MD; //Mission Duration
-	int consumptionDays; // The amount of days in execution for the mission 
+	int ED; //Execution Days: The amount of days in execution for the mission 
 	int leftDays; // the amount of days till the mission ends
+	int targetlocation;
+	int WD; //Waiting Days before the mission is executed
+	int CD; //Completion day is the finish time
 	// Equation of consumptionDays = 2*(TargetLocation*RoverSpeed)/25 +MD;
-	//
 	Rovers* R; //Rover assigned to the mission
 
 public:
-	Mission();
+	Mission(int fd, int md, int targetloc);
 	void assigntoRover(Rovers* R);
-
+	void setWaitingDays(int wd);
+	int getFinishDay() const;
 };
