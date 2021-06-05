dcf4d1e52de7195335778147bf1e468f7ae1ac91
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-05 22:35:19 +0200

    Update PRover.cpp

--- Rovers/PRover.cpp
+++ Rovers/PRover.cpp
@@ -2,13 +2,32 @@
 #include"Mission.h"
 #include "PRovers.h"
 
+PRover::PRover(int spd, int Checkupdur, int Ncheckup, int Mcomp, Mission* roverMission) : Rovers(spd, Checkupdur, Ncheckup, Mcomp)
+{
+	RoverMission = roverMission;
+
+}
 
 void PRover::setRoverMission(Mission a)
 {
-	Pmission = &a;
+	RoverMission = &a;
 }
 
 Mission* PRover:: getRoverMission()
 {
-	return Pmission;
+	return RoverMission;
+}
+
+bool PRover::isAssigned() //is rover is Assigend to a mission or no and this function is made for potential use in future 
+{
+	if (this->RoverMission)
+	{
+		RoverMission->setAssigned(true);
+		return true;
+	}
+	else
+	{
+		RoverMission->setAssigned(false);
+		return false;
+	}
 }
