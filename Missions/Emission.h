fdd53b340a60d54e96f00c32839ba2d77b15ced5
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-05 22:42:33 +0200

    Update Emission.h

--- Missions/Emission.h
+++ Missions/Emission.h
@@ -1,12 +1,16 @@
+pragma once
 #include "Mission.h"
 
-class Emission: public Mission
+class Emission :  public Mission
 {
-
 private:
 	double Priority;
 
 public:
-	Emission(int fd, int md, int targetloc);
-	friend bool operator>(const Emission & E1, const Emission & E2);
+	Emission();
+	Emission(int fd, int md, int targetloc,int Id);
+	void setPriority();
+	friend bool operator>(const Emission& E1, const Emission& E2);
+	int getPriority();
 };
+
