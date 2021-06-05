18124c5a3cc105df2a84fffce04740204f70bd06
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-05 22:40:25 +0200

    Update Mission.h

--- Missions/Mission.h
+++ Missions/Mission.h
@@ -1,10 +1,7 @@
 #pragma once
-
 #include "Rovers.h"
-
 class Mission
 {
-
 private:
 	int FD; //Formulation day of the mission
 	int MD; //Mission Duration
@@ -12,20 +9,30 @@ private:
 	int leftDays; // the amount of days till the mission ends
 	int targetlocation;
 	int WD; //Waiting Days before the mission is executed
+	int SWD; // start of the waiting days
+	int EWD; // end of the waiting days 
 	int CD; //Completion day is the finish time
 	int Significance;
 	// Equation of consumptionDays = 2*(TargetLocation*RoverSpeed)/25 +MD;
-	Rovers* R; //Rover assigned to the mission
+	//Rovers* R; //Rover assigned to the mission
+	bool Assigned;
 	char missionType;
 	int missionID;
 	static int numberOfmissions;
 
 public:
-	Mission(int fd, int md, int targetloc, int significance,int Id,char MT);
-	void assigntoRover(Rovers* R);
-	void setWaitingDays(int wd);
-	
+	Mission();
+	Mission(int fd, int md, int targetloc, int significance);
+	Mission(int fd, int md, int targetloc, int significance, int Id);
+	//void assigntoRover(Rovers* R);
+	void setWaitingDays();
+	void setAssigned(bool t);
+	bool getAssigned();
 
+	void setSWD(int x);
+	void setEWD(int y);
+	int getSWD();
+	int getEWD();
 	int getFinishDay() const;
 	int getNumberOfmissions() const;
 	int getFormulationDay() const;
