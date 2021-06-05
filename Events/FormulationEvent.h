32acf15083c3567a5d4c0c14ed91f790d717bc8f
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-05 22:47:13 +0200

    Update FormulationEvent.h

--- Events/FormulationEvent.h
+++ Events/FormulationEvent.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "Event.h"
-
-
 #pragma once
 #include "Event.h"
 class FormulationEvent :public Event
 {
 private:
-	float Tlocation; //the mission’s target location (in kilometers from the base station)
-	float missionDuration; // number of days needed to fulfill the mission requirements at target location
+	int Tlocation; //the mission’s target location (in kilometers from the base station)
+	int missionDuration; // number of days needed to fulfill the mission requirements at target location
 	int sig; // mission’s significance
 	char Mtype;
 public:
-	FormulationEvent(char m, int x, int y, float t, float f, int s);
-	Mission& Execute(int fd);
+	FormulationEvent(char m, int x, int y, int t, int f, int s);
+	//void Execute();
+	Mission* Execute(int fd);
+	Emission ExecuteEmg(int fd );
+	Pmission ExecutePol(int fd);
 	~FormulationEvent();
+
 };
