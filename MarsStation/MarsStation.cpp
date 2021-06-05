f6045831d449e58f5edc1c1e43709d5138538ab2
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-05 22:49:18 +0200

    Update MarsStation.cpp

--- MarsStation/MarsStation.cpp
+++ MarsStation/MarsStation.cpp
@@ -1,23 +1,59 @@
 #include "MarsStation.h"
+/*
 
+LinkedQueue<Event*> EventList;
+	LinkedQueue<Mission> PMissionList;
+	PriQ<Emission> EMissionList;
+	LinkedQueue<ERover> AvailableERoverList;
+	LinkedQueue<PRover> AvailablePRoverList;
+	PriQ<Rovers*> In_execution_rover_list;
+	LinkedQueue <Mission> CompletedMissionList;
+	LinkedQueue<ERover> CheckUpEmg;
+	LinkedQueue<PRover> CheckUpPolar;
+*/
 
 void MarsStation::ExecuteAll()
 {
-	while (!EventList.isEmpty())
+	while (!EventList.isEmpty() || !EMissionList.IsEmpty() || !PMissionList.isEmpty() || !In_execution_rover_list.IsEmpty())
 	{
 		Event* temp;
 		EventList.peek(temp);
-		if (temp->getday() == currentDay) //TODO getday() function in event classes
+		if (temp->getDay() == currentDay)
+		{
+				
+				while (temp->getDay() == currentDay)
+				{
+					Event* r = dynamic_cast<FormulationEvent*>(temp);
+					FormulationEvent* p = (FormulationEvent*)r;
+					if (r)
+					{
+						EventList.dequeue(temp);
+						Emission* e = (Emission*)p->Execute(currentDay);
+						if (e)
+						{
+							e->setSWD(currentDay);
+							EMissionList.enqueue(e);
+						}
+						else
+						{
+							Pmission* e = (Pmission*)p->Execute(currentDay);
+							e->setSWD(currentDay);
+							PMissionList.enqueue(*e);
+						}
+						EventList.peek(temp);
+					}
+				}
+			
+		}
+
+		while (!EMissionList.IsEmpty() && !(AvailableERoverList.isEmpty() || (AvailablePRoverList.isEmpty())))
 		{
 			
-			while (temp->getday() == currentDay)
-			{
-				EventList.dequeue(temp);
-				temp->Execute();
-				EventList.peek(temp);
-			}
+
+
 		}
-		userInterface.ConsoleOutput();//needs modifications for the arguments
-		++currentDay;
+
 	}
+	userInterface.ConsoleOutput();//needs modifications for the arguments
+	++currentDay;
 }
