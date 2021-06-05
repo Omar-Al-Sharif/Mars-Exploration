aee84ec953877838cf200761a2d72bc6f90fff78
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-05 22:44:50 +0200

    Add files via upload

---
+++ Missions/PMission.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "PMission.h"
+#include "Mission.h"
+Pmission::Pmission(int fd, int md, int targetloc, int Id) : Mission(fd, md, targetloc, Id)
+{
+	
+}
+
+Pmission::Pmission()
+{
+
+}
