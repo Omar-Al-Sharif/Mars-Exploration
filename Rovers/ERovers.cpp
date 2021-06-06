f593343c388b3f184d4db1a5c4812816c2f1d69b
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-06-06 20:21:49 +0200

    Update ERovers.cpp

--- Rovers/ERovers.cpp
+++ Rovers/ERovers.cpp
@@ -1,21 +1,12 @@
 #include"ERovers.h"
 #include"Mission.h"
 
-ERover:: ERover(int spd, int Checkupdur, int Ncheckup, int Mcomp, Mission* roverMission) : Rovers(spd, Checkupdur, Ncheckup, Mcomp)
+ERover:: ERover(int spd, int Checkupdur, int Ncheckup, int Mcomp, Mission* roverMission) : Rovers(spd, Checkupdur, Ncheckup, Mcomp, roverMission)
 {
-	roverMission = nullptr;
+
 }
 
 
-void ERover::setRoverMission(Mission a)
-{
-	RoverMission = &a;
-}
-
-Mission* ERover::getRoverMission()
-{
-	return RoverMission;
-}
 
 bool ERover::isAssigned() //is rover is Assigend to a mission or no and this function is made for potential use in future 
 {
