f9041b018ad91378e5658e6a6acfc336fb3f2412
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-07 00:33:28 +0200

    Update MarsStation.cpp

--- MarsStation/MarsStation.cpp
+++ MarsStation/MarsStation.cpp
@@ -1,20 +1,46 @@
 #include "MarsStation.h"
-/*
 
-LinkedQueue<Event*> EventList;
+/*
+LinkedQueue<Event> EventList;
 	LinkedQueue<Mission> PMissionList;
 	PriQ<Emission> EMissionList;
 	LinkedQueue<ERover> AvailableERoverList;
 	LinkedQueue<PRover> AvailablePRoverList;
-	PriQ<Rovers*> In_execution_rover_list;
+	PriQ<Rovers> In_execution_rover_list;
 	LinkedQueue <Mission> CompletedMissionList;
 	LinkedQueue<ERover> CheckUpEmg;
 	LinkedQueue<PRover> CheckUpPolar;
 */
 
+
+
+MarsStation::MarsStation()
+{
+	currentDay = 0;
+}
+
+void MarsStation::AssignMission(FormulationEvent* p)
+{
+	p->Execute(currentDay);
+	bool b;
+	char type = p->gettype();
+	if (type=='E')
+	{
+	    EMissionList.enqueue((p->getEmission()));
+		p->getEmission()->setSWD(currentDay);
+	}
+	else
+	{
+		Pmission* g = p->getPmission();
+		b= PMissionList.enqueue(g);
+		p->getEmission()->setSWD(currentDay);
+	}
+}
+
+
 void MarsStation::ExecuteAll()
 {
-	while (!EventList.isEmpty() || !EMissionList.IsEmpty() || !PMissionList.isEmpty() || !In_execution_rover_list.IsEmpty())
+	while (!EventList.isEmpty() || !EMissionList.IsEmpty() || !PMissionList.isEmpty() || !In_execution_rover_list.IsEmpty() || !CheckUpPolar.isEmpty() || !CheckUpPolar.isEmpty())
 	{
 		Event* temp;
 		EventList.peek(temp);
@@ -24,22 +50,11 @@ void MarsStation::ExecuteAll()
 				while (temp->getDay() == currentDay)
 				{
 					Event* r = dynamic_cast<FormulationEvent*>(temp);
-					FormulationEvent* p = (FormulationEvent*)r;
 					if (r)
 					{
+						FormulationEvent* p = (FormulationEvent*)r;
 						EventList.dequeue(temp);
-						Emission* e = (Emission*)p->Execute(currentDay);
-						if (e)
-						{
-							e->setSWD(currentDay);
-							EMissionList.enqueue(e);
-						}
-						else
-						{
-							Pmission* e = (Pmission*)p->Execute(currentDay);
-							e->setSWD(currentDay);
-							PMissionList.enqueue(*e);
-						}
+						AssignMission(p);
 						EventList.peek(temp);
 					}
 				}
@@ -54,6 +69,6 @@ void MarsStation::ExecuteAll()
 		}
 
 	}
-	userInterface.ConsoleOutput();//needs modifications for the arguments
+	userInterface->ConsoleOutput();//needs modifications for the arguments
 	++currentDay;
 }
