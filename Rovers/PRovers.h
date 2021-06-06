3aa4381f7dbbd32cfc1bd3d1c0fc4d5762c999e6
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-06-06 20:22:14 +0200

    Update PRovers.h

--- Rovers/PRovers.h
+++ Rovers/PRovers.h
@@ -6,12 +6,7 @@
 
 class PRover : public Rovers
 {
-private :
-	Mission* RoverMission;
-
 public :
 	PRover(int spd, int Checkupdur, int Ncheckup, int Mcomp, Mission* roverMission);
-	void setRoverMission(Mission a);
-	Mission* getRoverMission();
 	bool isAssigned();
 };
