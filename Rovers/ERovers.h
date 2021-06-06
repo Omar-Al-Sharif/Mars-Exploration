9b0d76bcd7ec357ce92f5b54530da21009fe4aea
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-06-06 20:21:17 +0200

    Update ERovers.h

--- Rovers/ERovers.h
+++ Rovers/ERovers.h
@@ -6,14 +6,9 @@
 
 class ERover : public Rovers
 {
-private:
-	Mission* RoverMission;
-
 
 public:
 	ERover(int spd, int Checkupdur, int Ncheckup, int Mcomp, Mission* roverMission);
-	void setRoverMission(Mission a);
-	Mission* getRoverMission();
 	bool isAssigned();
 
 };
