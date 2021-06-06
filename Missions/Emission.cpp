248ca7fb6423b7c5edbc2bc56b7bfbd214ca71d7
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-06-07 00:53:55 +0200

    Update Emission.cpp

--- Missions/Emission.cpp
+++ Missions/Emission.cpp
@@ -1,13 +1,20 @@
 #include "Emission.h"
 
-Emission::Emission(int fd, int md, int targetloc, int significance, int Id) : Mission(fd, md, targetloc,significance, Id)
+#define Significance_Weight 100
+#define TargetLoc_Weight 0.01
+
+Emission::Emission(int fd, int md, int targetloc, int significance, int Id) : Mission(fd, md, targetloc, significance, Id)
 {
 	setPriority();
 }
 
 void Emission::setPriority()
 {
-	//write the piroity equation  
+	int TargetLoc = getTargetLoc();
+	int Significance = getSignificance();
+	int MD = getMissionDuration();
+	int FD = getFormulationDay();
+	Priority =  (Significance_Weight * Significance / (TargetLoc_Weight * TargetLoc * MD * FD) );
 }
 
 int Emission::getPriority() const
