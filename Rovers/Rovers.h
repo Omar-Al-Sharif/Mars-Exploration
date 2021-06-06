24e33f0236d258fae0fcd8d7a6ca89b170385050
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-06-06 20:19:59 +0200

    Update Rovers.h

--- Rovers/Rovers.h
+++ Rovers/Rovers.h
@@ -1,7 +1,9 @@
 #pragma once
+#include "Mission.h"
 
 // contains 3 lists :one for m , p , e
 
+class Mission;
 
 class Rovers
 {
@@ -10,16 +12,20 @@ protected:
 	int checkUpDur; //in days and it's the time needed for the rover to be checked 
 	int NeedCheckUP; // after how many missions I need to complete to make a checkup
 	int  MissionComp; //number of missions completed by the rover, we need it to know it for checkup
-	
+
+	Mission* RoverMission;
+
 public:
-	 Rovers(int spd, int Checkupdur, int Ncheckup, int Mcomp);
-	 void setSpeed(int a);
-	 void setCheckUpDur(int a);
-	 void setMissionComp(int a);
-	 void setNeedCheckUp (int a);
-	 int getMissionComp();
-	 int getNeedCheckUp();
-	 int getCheckUpDur();
-	 int getSpeed();
-	 ~Rovers();
+	Rovers(int spd, int Checkupdur, int Ncheckup, int Mcomp, Mission* roverMission);
+	void setSpeed(int a);
+	void setCheckUpDur(int a);
+	void setMissionComp(int a);
+	void setNeedCheckUp(int a);
+	int getMissionComp();
+	int getNeedCheckUp();
+	int getCheckUpDur();
+	int getSpeed();
+	Mission* getRoverMission();
+	~Rovers();
+	void setRoverMission(Mission a);
 };
