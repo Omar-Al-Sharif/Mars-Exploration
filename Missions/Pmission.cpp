3b58b7ab1acf45a2828566fc2eaf271160a9e195
Grintaking19 <69204285+Grintaking19@users.noreply.github.com>
2021-06-07 17:29:15 +0200

    Update Pmission.cpp

--- Missions/Pmission.cpp
+++ Missions/Pmission.cpp
@@ -1,8 +1,8 @@
 #pragma once
 #include "PMission.h"
-#include "Mission.h"
 Pmission::Pmission(int fd, int md, int targetloc, int Id, int signficance) : Mission(fd, md, targetloc, Id, signficance)
 {
-	
+	setMissionType('P');
 }
+
 
