48b483a04b4c23667916ecf41eecdb7cb717c407
omarelnahas <76655467+omarelnahas@users.noreply.github.com>
2021-06-07 17:56:15 +0200

    Add files via upload

---
+++ Rovers/Mrover.cpp
@@ -0,0 +1,6 @@
+#include "Mrover.h"
+
+Mrover::Mrover(int spd, int Checkupdur, int Ncheckup) : Rovers(spd, Checkupdur, Ncheckup)
+{
+
+}
---
+++ Rovers/Mrover.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include "Rovers.h"
+#include"../Data structure and algorithm/Mission.h"
+class Mrover : public Rovers
+{
+public:
+	Mrover(int spd, int Checkupdur, int Ncheckup);
+};
